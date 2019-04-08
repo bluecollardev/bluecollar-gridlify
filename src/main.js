c51229b86233fddc126e2ae28cc56f1dfd23495e
Lucas <lucas@Silver-Surfer.local>
2019-04-07 19:54:12 -0700

    Merge in ampstart styles.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import DefaultLayout from '~/layouts/Default.vue'
-import '~/assets/css/main'
+import '~/assets/css/main.scss'
 
 export default function (Vue, { head }) {
   head.style.push({ rel: 'stylesheet', href: 'https://unpkg.com/ace-css/css/ace.min.css' })
