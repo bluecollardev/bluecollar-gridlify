b79c2f8ab44df1a60a3518a4979cc19048d0b1b8
Lucas <lucas@Silver-Surfer.local>
2019-04-07 21:33:22 -0700

    Move window event listeners in Header component to mounted.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -86,22 +86,23 @@
     });
   }
 
-  window.addEventListener("resize", function(event) {
-    close_all_menu();
-    document.getElementsByTagName("body")[0].classList.remove("display_menu");
-  });
-  var last_scroll = 0;
-  window.onscroll = function() {
-    if (!document.getElementById("loader")) {
-      close_all_menu();
-      var header = document.getElementsByTagName("header")[0];
-      if (Math.abs(last_scroll - this.scrollY) <= 5) return;
-      (this.scrollY < last_scroll) ? header.style.top = "0": header.style.top = "-" + header.clientHeight + "px";
-      last_scroll = this.scrollY;
-    }
-  }
-
   export default {
+    mounted() {
+      window.addEventListener("resize", function(event) {
+        close_all_menu();
+        document.getElementsByTagName("body")[0].classList.remove("display_menu");
+      });
+      var last_scroll = 0;
+      window.onscroll = function() {
+        if (!document.getElementById("loader")) {
+          close_all_menu();
+          var header = document.getElementsByTagName("header")[0];
+          if (Math.abs(last_scroll - this.scrollY) <= 5) return;
+          (this.scrollY < last_scroll) ? header.style.top = "0": header.style.top = "-" + header.clientHeight + "px";
+          last_scroll = this.scrollY;
+        }
+      }
+    },
     methods: {
       display_menu: function() {
         var body = document.getElementsByTagName('body')[0];
