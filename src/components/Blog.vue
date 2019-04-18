b3687e79c6cc7c43e03514190cbaaf5557ea481d
Lucas <tech@firebrandwebsolutions.com>
2019-04-18 06:53:24 -0700

    Get blog working.

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,13 +1,40 @@
 <template>
-  <!--<ul id="blog">
-    <li v-for="{ node } in $static.allBlogPost.edges" :key="node.id">
-      <g-link :to="node.path">
-        <h6 v-html="node.title"/>
-      </g-link>
-      <span v-html="node.date"/>
-    </li>
-  </ul>-->
+  <section class="lg-flex flex-center flex-justify-center">
+    <div class="sm-flex flex-wrap flex-center flex-justify-center flex-basis-two-thirds">
+      <article class="hero-block" v-for="{ node } in $static.allBlogPost.edges" :key="node.id">
+        <content-block-layout
+          className="pad-top pad-bottom"
+          :title="node.title"
+          :description="node.content"
+          :link="node.path"
+          linkText="Read More"
+        />
+      </article>
+    </div>
+  </section>
 </template>
+
+<script>
+  // Import components
+  // Import generic component layouts
+  import HeroLayout02Col02 from '~/components/layouts/HeroLayout02Col02.vue';
+  import SectionBlockLayout02Col01 from '~/components/layouts/SectionBlockLayout02Col01.vue';
+  import SectionBlockLayout02Col02 from '~/components/layouts/SectionBlockLayout02Col02.vue';
+  import ContentBlockLayout from '~/components/layouts/ContentBlockLayout.vue';
+  import TestimonialBlockLayout from '~/components/layouts/TestimonialBlockLayout.vue';
+
+  export default {
+    components: {
+      // Inject components
+      // Inject generic component layouts
+      HeroLayout02Col02,
+      SectionBlockLayout02Col01,
+      SectionBlockLayout02Col02,
+      ContentBlockLayout,
+      TestimonialBlockLayout,
+    }
+  }
+</script>
 
 <static-query>
   query Blog {
@@ -18,6 +45,7 @@
           title
           date (format: "YYYY")
           path
+          content
         }
       }
     }
--- src/templates/BlogPost.vue
+++ src/templates/BlogPost.vue
@@ -1,13 +1,20 @@
 <template>
   <Layout>
     <Header/>
-    <div class="article">
-      <div class="meta">
-        <h1>{{ $page.blogPost.title }}</h1>
-        <span>{{ $page.blogPost.date }}</span>
+    <section class="hero-section sm-flex space-top-2x space-bottom-2x pad-top pad-bottom">
+      <div class="sm-flex flex-auto flex-center flex-justify-center">
+        <div class="content-block">
+          <div>
+            <div class="space-bottom">
+              <p class="g-pstyle2 text-center-force">{{ $page.blogPost.title }}</p>
+            </div>
+            <div class="g-banner">
+              <div v-html="$page.blogPost.content"></div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="article__content" v-html="$page.blogPost.content" />
-    </div>
+    </section>
     <Footer/>
   </Layout>
 </template>
