3139644f0f694e121a24590127974cf031185968
Lucas <tech@firebrandwebsolutions.com>
2019-04-25 15:18:49 -0700

    Add disclaimer.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,7 +1,60 @@
 <template>
   <main>
     <div class="container">
+      <div class="notifications" v-if="displayNotifications">
+        <span class="notification">
+          <span>ⓘ Disclaimer - this site is under construction and is intended as the personal portfolio of Lucas Lopatka. In-accuracies do exist and some projects are not correctly described, and working relationships have not been identified as contract or employee. Employee relationships will be moved to lucaslopatka.com. I am currently working on customizing the project repeater as to distinguish between client and employee projects, and password protect the project's section. If you have any issue with the content of this site, contact me at lucas@bluecollardev.com.</span>
+          <button class="hide-notification" aria-label="Hide notification" v-on:click="hideNotifications()">&times;</button>
+        </span>
+      </div>
       <slot/>
     </div>
   </main>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        displayNotifications: true
+      }
+    },
+    methods: {
+      hideNotifications() {
+        this.displayNotifications = false
+      }
+    }
+  }
+</script>
+
+<style>
+.notifications {
+  position: absolute;
+  display: flex;
+  align-items: flex-start;
+  top: 48px;
+  height: auto;
+  width: 100vw;
+  z-index: 100;
+}
+
+.notification {
+  display: flex;
+  align-items: flex-start;
+  padding: 0.5rem 14rem;
+  color: white;
+  font-weight: bold;
+  font-size: 80%;
+  text-align: justify;
+  background-color: #3d9970;
+}
+
+.hide-notification {
+  background-color: transparent;
+  border: none;
+  font-size: 2rem;
+  color: white;
+  margin-left: 1rem;
+  cursor: pointer;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import DefaultLayout from '~/layouts/Default.vue'
-import '~/assets/css/main.scss'
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
+import DefaultLayout from '~/layouts/Default.vue';
+import '~/assets/css/main.scss';
 
 export default function (Vue, { head }) {
-  head.style.push({ rel: 'stylesheet', href: 'https://unpkg.com/ace-css/css/ace.min.css' })
-  Vue.component('Layout', DefaultLayout)
+  head.link.push({ rel: 'stylesheet', href: 'https://unpkg.com/ace-css/css/ace.min.css' });
+  // Set default layout as a global component
+  Vue.component('Layout', DefaultLayout);
 }
