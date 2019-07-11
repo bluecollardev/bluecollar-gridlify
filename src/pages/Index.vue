7cbc49ec1dfb27b8f84d1fd1aabf3fed2716e9fb
Lucas <tech@firebrandwebsolutions.com>
2019-07-11 13:55:36 -0700

    Responsive form tweaks.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -376,132 +376,135 @@
                 </div>
 
                 <div class="md-lg-flex flex-justify-center space-top">
+                  <div class="top-fields sm-flex flex-column flex-basis-third flex-start">
+                    <div>
+                      <!-- Start Input -->
+                      <div class="ampstart-input relative m0 p0 mb3 ">
+                        <input
+                          type="text"
+                          value=""
+                          name="fullname"
+                          id="fullname"
+                          class="block border-none p0 m0"
+                          placeholder="Full name"
+                          v-model="formData.fullname"
+                          required
+                        />
+                        <label
+                          for="fullname"
+                          class="absolute top-0 right-0 bottom-0 left-0"
+                          aria-hidden="true"
+                          >Full name</label
+                        >
+                      </div>
+                      <!-- End Input-->
 
-                  <div class="sm-flex flex-column flex-basis-third flex-start">
-                    <!-- Start Input -->
-                    <div class="ampstart-input relative m0 p0 mb3 ">
-                      <input
-                        type="text"
-                        value=""
-                        name="fullname"
-                        id="fullname"
-                        class="block border-none p0 m0"
-                        placeholder="Full name"
-                        v-model="formData.fullname"
-                        required
-                      />
-                      <label
-                        for="fullname"
-                        class="absolute top-0 right-0 bottom-0 left-0"
-                        aria-hidden="true"
-                        >Full name</label
-                      >
-                    </div>
-                    <!-- End Input-->
+                      <!-- Start Input -->
+                      <div class="ampstart-input relative m0 p0 mb3 ">
+                        <input
+                          type="tel"
+                          value=""
+                          name="phone"
+                          id="phone"
+                          class="block border-none p0 m0"
+                          placeholder="Phone"
+                          v-model="formData.phone"
+                          required
+                        />
+                        <label
+                          for="phone"
+                          class="absolute top-0 right-0 bottom-0 left-0"
+                          aria-hidden="true"
+                          >Phone</label
+                        >
+                      </div>
+                      <!-- End Input-->
 
-                    <!-- Start Input -->
-                    <div class="ampstart-input relative m0 p0 mb3 ">
-                      <input
-                        type="tel"
-                        value=""
-                        name="phone"
-                        id="phone"
-                        class="block border-none p0 m0"
-                        placeholder="Phone"
-                        v-model="formData.phone"
-                        required
-                      />
-                      <label
-                        for="phone"
-                        class="absolute top-0 right-0 bottom-0 left-0"
-                        aria-hidden="true"
-                        >Phone</label
-                      >
+                      <!-- Start Input -->
+                      <div class="ampstart-input relative m0 p0 mb3 ">
+                        <input
+                          type="email"
+                          value=""
+                          name="email"
+                          id="email"
+                          class="block border-none p0 m0"
+                          placeholder="Email"
+                          v-model="formData.email"
+                          required
+                        />
+                        <label
+                          for="email"
+                          class="absolute top-0 right-0 bottom-0 left-0"
+                          aria-hidden="true"
+                          >Email</label
+                        >
+                      </div>
+                      <!-- End Input-->
                     </div>
-                    <!-- End Input-->
 
-                    <!-- Start Input -->
-                    <div class="ampstart-input relative m0 p0 mb3 ">
-                      <input
-                        type="email"
-                        value=""
-                        name="email"
-                        id="email"
-                        class="block border-none p0 m0"
-                        placeholder="Email"
-                        v-model="formData.email"
-                        required
-                      />
-                      <label
-                        for="email"
-                        class="absolute top-0 right-0 bottom-0 left-0"
-                        aria-hidden="true"
-                        >Email</label
-                      >
-                    </div>
-                    <!-- End Input-->
+                    <div>
+                      <!-- Start Input -->
+                      <div class="ampstart-input relative m0 p0 mb3 ">
+                        <input
+                          type="text"
+                          value=""
+                          name="budget"
+                          id="budget"
+                          class="block border-none p0 m0"
+                          placeholder="Project Budget"
+                          v-model="formData.budget"
+                          required
+                        />
+                        <label
+                          for="budget"
+                          class="absolute top-0 right-0 bottom-0 left-0"
+                          aria-hidden="true"
+                          >Project Budget</label
+                        >
+                      </div>
+                      <!-- End Input-->
 
-                    <!-- Start Input -->
-                    <div class="ampstart-input relative m0 p0 mb3 ">
-                      <input
-                        type="text"
-                        value=""
-                        name="budget"
-                        id="budget"
-                        class="block border-none p0 m0"
-                        placeholder="Project Budget"
-                        v-model="formData.budget"
-                        required
-                      />
-                      <label
-                        for="budget"
-                        class="absolute top-0 right-0 bottom-0 left-0"
-                        aria-hidden="true"
-                        >Project Budget</label
-                      >
-                    </div>
-                    <!-- End Input-->
+                      <!-- Start Input -->
+                      <div class="ampstart-input relative m0 p0 mb3 ">
+                        <input
+                          type="text"
+                          value=""
+                          name="city"
+                          id="city"
+                          class="block border-none p0 m0"
+                          placeholder="City"
+                          v-model="formData.city"
+                          required
+                        />
+                        <label
+                          for="city"
+                          class="absolute top-0 right-0 bottom-0 left-0"
+                          aria-hidden="true"
+                          >City</label
+                        >
+                      </div>
+                      <!-- End Input-->
 
-                    <!-- Start Input -->
-                    <div class="ampstart-input relative m0 p0 mb3 ">
-                      <input
-                        type="text"
-                        value=""
-                        name="city"
-                        id="city"
-                        class="block border-none p0 m0"
-                        placeholder="City"
-                        v-model="formData.city"
-                        required
-                      />
-                      <label
-                        for="city"
-                        class="absolute top-0 right-0 bottom-0 left-0"
-                        aria-hidden="true"
-                        >City</label
-                      >
+                      <!-- Start Input -->
+                      <div class="ampstart-input relative m0 p0 mb3 ">
+                        <input
+                          type="text"
+                          value=""
+                          name="country"
+                          id="country"
+                          class="block border-none p0 m0"
+                          placeholder="Country"
+                          v-model="formData.country"
+                        />
+                        <label
+                          for="country"
+                          class="absolute top-0 right-0 bottom-0 left-0"
+                          aria-hidden="true"
+                          >Country</label
+                        >
+                      </div>
+                      <!-- End Input-->
                     </div>
-                    <!-- End Input-->
-
-                    <!-- Start Input -->
-                    <div class="ampstart-input relative m0 p0 mb3 ">
-                      <input
-                        type="text"
-                        value=""
-                        name="country"
-                        id="country"
-                        class="block border-none p0 m0"
-                        placeholder="Country"
-                        v-model="formData.country"
-                      />
-                      <label
-                        for="country"
-                        class="absolute top-0 right-0 bottom-0 left-0"
-                        aria-hidden="true"
-                        >Country</label
-                      >
-                    </div>
-                    <!-- End Input-->
                   </div>
 
                   <div class="sm-flex flex-column flex-basis-third flex-start">
