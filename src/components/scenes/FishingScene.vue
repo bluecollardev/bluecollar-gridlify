14a58e7f89d018d9cde45ff5e07c6426042cbc5b
Lucas <tech@firebrandwebsolutions.com>
2019-08-09 18:54:57 -0700

    Update fishing scene.

--- src/components/scenes/FishingScene.vue
+++ src/components/scenes/FishingScene.vue
@@ -9,52 +9,71 @@
   import anime from 'animejs';
 
   export default {
+    props: {
+      parabolicFx: {
+        type: Function,
+        default: (x, vtx) => -0.001 * Math.pow(x - vtx.x, 2) + vtx.y
+      }
+    },
     methods: {
-      animateOnParabolicPath(el, fx, cb) {
+      setFishRotation(el, { x, y}) {
+        const xMax = 1400;
+        const startRotation = -65;
+        const completeRotation = 55;
+        const rotationDelta = completeRotation - startRotation;
+
+        console.log(`current coords: ${x}, ${y}`);
+        // Get x coord % of max
+        let xPct = x / xMax;
+        console.log(`x pct: ${xPct * 100}`);
+
+        el.style.transform = `rotate(${ startRotation + (rotationDelta * xPct) }deg) scaleX(-1)`;
+      },
+      animateOnParabolicPath(el, fx, vtx, cb) {
         if (typeof window !== 'undefined') {
           let x = 20;
-          const timer = setInterval(() => {
+          let requestId = null;
+
+          const updatePosition = () => {
             //console.log(`test | y: ${fx(x)}, x: ${x}`);
+            const y = fx(x, vtx);
 
-            el.style.bottom = fx(x) + 'px';
-            el.style.left = x + 'px';
+            el.style.bottom =  `${y}px`;
+            el.style.left = `${x}px`;
 
-            cb({ x, y: fx(x) });
+            cb(el, { x, y });
 
-            // 100 is a fudge factor to allow element to clear off screen
-            if (x > 800 + 800) clearInterval(timer);
+            // Second 800 is a fudge factor to allow element to clear off screen
+            if (x > 800 + 800) (cancelAnimationFrame(id));
             x += 20;
-          }, 33);
-        }
-      },
-      play() {
-        if (typeof window !== 'undefined') {
-          const el = document.querySelector('.fish');
-          const xMax = 1400;
-          const xVertex = 700;
-          const yVertex = 500;
-          const startRotation = -65;
-          const completeRotation = 55;
-          const rotationDelta = completeRotation - startRotation;
 
-          let elRotation = startRotation;
+            requestId = requestAnimationFrame(updatePosition)
+          };
 
-          this.animateOnParabolicPath(el, (x) => -0.001 * Math.pow(x - xVertex, 2) + yVertex, ({ x, y }) => {
-            console.log(`current coords: ${x}, ${y}`);
-            // Get x coord % of max
-            let xPct = x / xMax;
-            console.log(`x pct: ${xPct * 100}`);
-
-            el.style.transform = `rotate(${ startRotation + (rotationDelta * xPct) }deg) scaleX(-1)`;
-          });
+          requestId = requestAnimationFrame(updatePosition);
         }
       },
       reelInFish() {
-        this.play();
+        this.doFishJump();
+      },
+      doFishJump() {
+        if (typeof window !== 'undefined') {
+          const el = document.querySelector('.fish');
+
+          const xVertex = 700;
+          const yVertex = 500;
+
+          // requestAnimationFrame
+          this.animateOnParabolicPath(el,
+            this.parabolicFx,
+            { x: xVertex, y: yVertex },
+            this.setFishRotation
+          );
+        }
       }
     },
     mounted() {
-      const timer = setInterval(this.play, 8000);
+      //this.play();
     }
   }
 </script>
