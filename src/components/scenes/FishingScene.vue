38ba54a1639a1f0434e7ed80de7cc4eb9c4b632b
Lucas <tech@firebrandwebsolutions.com>
2019-08-11 21:56:05 -0700

    Work on fishing line animations.

--- src/components/scenes/FishingScene.vue
+++ src/components/scenes/FishingScene.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="flex-basis-half flex flex-column">
-    <img ref="fishingRod" @click="doCast()" :class="`fishing-rod ${isCasting ? 'is-casting' : isOnHook ? 'fish-is-attached' : 'idle'}`" src="/images/fishing-rod.svg" />
+    <span ref="fishingRod" :class="`fishing-rod-object ${isCasting ? 'is-casting' : isOnHook ? 'fish-is-attached' : 'idle'}`">
+      <span class="line-anchor" style="width: 6px; height: 6px; background: black; display: block;"></span><img @click="doCast()" :class="`fishing-rod`" src="/images/fishing-rod.svg" />
+    </span>
     <img ref="redSnapper" class="red-snapper fish" src="/images/red-snapper.svg" />
     <img ref="fishCaught" class="fish-caught fish" src="/images/red-snapper.svg" />
+    <div id="casting-line"></div>
+    <div id="reeling-line"></div>
+
   </div>
 </template>
 
 <script>
   import anime from 'animejs';
+  import SVG from 'svg.js';
 
   export default {
     props: {
@@ -64,6 +70,8 @@
         }
       },
       animateOnLine(el, fx, coords, cb) {
+        this.drawReelingLine();
+
         if (typeof window !== 'undefined') {
           let requestId = null;
           let y = coords[0];
@@ -120,6 +128,8 @@
         }
       },
       doCast() {
+        this.clearReelingLine();
+
         const fishCaught = document.querySelector('.fish-caught');
         fishCaught.style.right = '19%';
         fishCaught.style.bottom = '-350px';
@@ -127,26 +137,76 @@
         this.$set(this, 'isCasting', true);
         this.$set(this, 'isOnHook', false);
 
-        const fishingRod = document.querySelector('.fishing-rod'); //this.$refs.fishingRod;
+        const fishingRod = document.querySelector('.fishing-rod-object'); //this.$refs.fishingRod;
         fishingRod.addEventListener('webkitAnimationEnd', this.resetCast);
         fishingRod.addEventListener('oAnimationEnd', this.resetCast);
         fishingRod.addEventListener('msAnimationEnd', this.resetCast);
         fishingRod.addEventListener('animationend', this.resetCast);
+
+        setTimeout(this.drawCastingLine, 2300); // Same time as css animation
       },
       resetCast() {
         // Something is making this animationend trigger fire waaaay too fast...
         setTimeout(() => {
           //this.$set(this, 'isCasting', false);
 
-          const fishingRod = document.querySelector('.fishing-rod'); //this.$refs.fishingRod;
+          const fishingRod = document.querySelector('.fishing-rod-object'); //this.$refs.fishingRod;
           fishingRod.removeEventListener('webkitAnimationEnd', this.resetCast);
           fishingRod.removeEventListener('oAnimationEnd', this.resetCast);
           fishingRod.removeEventListener('msAnimationEnd', this.resetCast);
           fishingRod.removeEventListener('animationend', this.resetCast);
 
+          this.clearCastingLine();
           this.doFishCaught();
           //this.doFishJump();
         }, 3000);
+      },
+      drawCastingLine() {
+        const fishingRodCoords = document.querySelector('.line-anchor').getBoundingClientRect();
+        const a = 55 * Math.PI / 180;
+        //const startY = fishingRodCoords.y * Math.cos(a) - fishingRodCoords.x * Math.sin(a);
+        //const startX = fishingRodCoords.y * Math.sin(a) + fishingRodCoords.x * Math.cos(a);
+
+        const startY = fishingRodCoords.y;
+        const startX = fishingRodCoords.x;
+
+        //console.log(`orig x: ${fishingRodCoords.x}, y: ${fishingRodCoords.y}`);
+        console.log(`start x: ${startX}, y: ${startY}`);
+
+        const castingLine = SVG('casting-line');
+        castingLine.size(document.body.clientWidth, 800);
+
+        castingLine.path(`M ${startX} ${startY} S ${startX - 300},${startY} ${startX - 500}, 600`)
+          .fill('none').stroke({ color: '#000', width: 1 });
+      },
+      drawReelingLine() {
+        const fishingRodCoords = document.querySelector('.line-anchor').getBoundingClientRect();
+        const fishCaughtCoords = document.querySelector('.fish-caught').getBoundingClientRect();
+
+        const endY = fishCaughtCoords.y + 1000;
+        const endX = fishCaughtCoords.x + 105;
+
+        const startY = fishingRodCoords.y - 300;
+        const startX = endX;
+
+        console.log(`start x: ${startX}, y: ${startY}`);
+        console.log(`end x: ${endX}, y: ${endY}`);
+
+        const reelingLine = SVG('reeling-line');
+        reelingLine.size(document.body.clientWidth, 800);
+
+        reelingLine.path(`M ${startX} ${startY} L ${endX} ${endY}`)
+          .fill('none').stroke({ color: '#000', width: 1 });
+      },
+      clearCastingLine() {
+        if (document.querySelectorAll('#casting-line svg').length > 0) {
+          document.querySelector('#casting-line').removeChild(document.querySelector('#casting-line svg'));
+        }
+      },
+      clearReelingLine() {
+        if (document.querySelectorAll('#reeling-line svg').length > 0) {
+          document.querySelector('#reeling-line').removeChild(document.querySelector('#reeling-line svg'));
+        }
       }
     },
     mounted() {
@@ -160,14 +220,59 @@
 </script>
 
 <style lang="scss">
-  .fishing-rod {
+  #casting-line, #reeling-line {
+    position: absolute;
+    width: 100%;
+    height: 800px;
+    left: 0;
+    top: 0;
+    z-index: -1;
+  }
+
+  #casting-line {
+    path {
+      stroke-dasharray: 1000;
+      stroke-dashoffset: 1000;
+      animation: casting_line 1s linear alternate;
+      animation-fill-mode: forwards;
+    }
+  }
+
+  #reeling-line {
+    path {
+      stroke-dasharray: 1000;
+      stroke-dashoffset: 1000;
+      animation: reeling_line 5s linear alternate;
+      animation-fill-mode: forwards;
+    }
+  }
+
+  @keyframes casting_line {
+    from {
+      stroke-dashoffset: 1000;
+    }
+    to {
+      stroke-dashoffset: 0;
+    }
+  }
+
+  @keyframes reeling_line {
+    from {
+      stroke-dashoffset: 0;
+    }
+    to {
+      stroke-dashoffset: 300;
+    }
+  }
+
+  .fishing-rod-object {
     position: relative;
     left: 200px;
     top: 550px;
     transform: rotate(35deg);
     transform-origin: bottom right;
     cursor: pointer;
-    animation-name: fishing_rod_hover;
+    //animation-name: fishing_rod_hover;
     animation-duration: 3.5s;
     animation-iteration-count: infinite;
 
