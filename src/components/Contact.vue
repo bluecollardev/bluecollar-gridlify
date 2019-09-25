07ceacf154875c0cbeb125134418eb45c43f4310
Lucas <tech@firebrandwebsolutions.com>
2019-09-25 00:46:33 -0700

    Update contact text.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -16,12 +16,16 @@
 				text="Get in Touch"
 				class="title text-center pad-bottom">
 			</typewriter-text-effect>
-			<h2 class="block text-center mb-4 space-top">
-
-			</h2>
 			<div class="hide">
 				<label>Hello bee:</label>
 				<input name="bot-field" />
+			</div>
+
+			<div class="relative flex xs-flex-basis-full sm-flex-basis-full md-lg-flex-basis-full lg-flex-basis-half xl-flex-basis-half flex-justify-center flex-center pad-top">
+				<div>
+					<p class="text-center">We're located in Victoria, BC, Canada, providing IT consulting services for customers in Victoria and the BC Lower Mainland. We're open to remote work.</p>
+					<p class="text-center">If you'd like to get in touch with a developer, contact Lucas at 250-532-0083.</p>
+				</div>
 			</div>
 
 			<div class="md-lg-flex flex-justify-center space-top">
