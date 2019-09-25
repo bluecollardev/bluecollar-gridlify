14962166cee940a7e9a3a33b6e86e4fdaf4bbf68
Lucas <tech@firebrandwebsolutions.com>
2019-09-24 19:51:47 -0700

    Remove stuff from footer that we're never gonna use.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,68 +2,12 @@
   <footer class="flex flex-column">
     <div class="sm-flex space-top flex-wrap space-bottom">
       <div class="sm-flex xs-flex-basis-full sm-flex-basis-full md-flex-basis-full md-lg-flex-basis-three-quarters lg-flex-basis-three-quarters space-bottom">
-        <div class="flex-basis-third sm-flex sm-flex-justify-left">
-          <div>
-            <div class="g-footer space-bottom">
-              <p class="g-pstyle13"><a href="/#services">Development</a></p>
-            </div>
-            <div class="g-footer">
-              <p class="g-pstyle7"><a href="/#services">Hybrid Apps + PWAs</a></p>
-
-              <p class="g-pstyle7"><a href="/#services">Custom Software</a></p>
-
-              <p class="g-pstyle7"><a href="/#services">S.P.A. Websites</a></p>
-            </div>
+        <div class="flex-basis-two-thirds sm-flex sm-flex-justify-left">
+          <div class="sm-flex flex-justify-between">
+          <div class="copyright g-footer">
+            <p class="g-pstyle15">&copy; Blue Collar Development Ltd. 2019<br /><small><small>Site design and development by Lucas Lopatka</small></small></p>
           </div>
         </div>
-
-        <!-- Just leaving this in here for reference if I want to re-introduce these services -->
-        <!--<div class="flex-basis-third flex flex-justify-center">
-          <div>
-            <div class="g-footer space-bottom">
-              <p class="g-pstyle13"><a href="/services-digital-marketing">Marketing</a></p>
-            </div>
-            <div class="g-footer">
-              <p class="g-pstyle7"><a href="/services-landing-pages">Landing Pages</a></p>
-
-              <p class="g-pstyle7"><a href="/services-digital-marketing#social-media">Social Media</a></p>
-
-              <p class="g-pstyle7"><a href="/services-digital-marketing#advertising">Advertising</a></p>
-            </div>
-          </div>
-        </div>-->
-
-        <div class="flex-basis-third sm-flex sm-flex-justify-center">
-          <div>
-            <div class="g-footer space-bottom">
-              <p class="g-pstyle13"><a href="/#top">Company</a></p>
-            </div>
-            <div class="g-footer">
-              <p class="g-pstyle7"><a href="/#top">The Blue Collar Way</a></p>
-
-              <p class="g-pstyle7"><a href="/">Consulting Services</a></p>
-
-              <p class="g-pstyle7"><a href="/">Junior Dev Program</a></p>
-            </div>
-          </div>
-        </div>
-
-        <div class="flex-basis-third sm-flex sm-flex-justify-center xl-flex-justify-end">
-          <div>
-            <div class="g-footer space-bottom">
-              <p class="g-pstyle13">Resources</p>
-            </div>
-            <div class="g-footer">
-              <p class="g-pstyle7"><a href="/#">Blog (Coming Soon)</a></p>
-            </div>
-            <div class="g-footer">
-              <p class="g-pstyle7"><a href="https://github.com/bluecollardev">Open Source Projects</a></p>
-            </div>
-            <div class="g-footer">
-              <p class="g-pstyle7"><a href="https://github.com/bluecollardev/bluecollar-gridlify">Source Code</a></p>
-            </div>
-          </div>
-
         </div>
       </div>
       <div class="sm-flex xs-flex-basis-full sm-flex-basis-full md-flex-basis-full md-lg-flex-basis-quarter lg-flex-basis-quarter xl-flex-basis-quarter flex-justify-end">
@@ -79,25 +23,6 @@
               <a href="https://www.facebook.com/bluecollardev/" target="_blank" class="inline-block p1" aria-label="Link to Twitter"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="44.4" viewBox="0 0 53 49"><title>Twitter</title><path d="M45 6.9c-1.6 1-3.3 1.6-5.2 2-1.5-1.6-3.6-2.6-5.9-2.6-4.5 0-8.2 3.7-8.2 8.3 0 .6.1 1.3.2 1.9-6.8-.4-12.8-3.7-16.8-8.7C8.4 9 8 10.5 8 12c0 2.8 1.4 5.4 3.6 6.9-1.3-.1-2.6-.5-3.7-1.1v.1c0 4 2.8 7.4 6.6 8.1-.7.2-1.5.3-2.2.3-.5 0-1 0-1.5-.1 1 3.3 4 5.7 7.6 5.7-2.8 2.2-6.3 3.6-10.2 3.6-.6 0-1.3-.1-1.9-.1 3.6 2.3 7.9 3.7 12.5 3.7 15.1 0 23.3-12.6 23.3-23.6 0-.3 0-.7-.1-1 1.6-1.2 3-2.7 4.1-4.3-1.4.6-3 1.1-4.7 1.3 1.7-1 3-2.7 3.6-4.6" class="ampstart-icon ampstart-icon-twitter"></path></svg></a>
             </li>
           </ul>
-        </div>
-      </div>
-    </div>
-
-    <hr style="width: 100%" />
-
-    <div class="space-top space-bottom">
-      <!--<div class="flex flex-center xs-flex-justify-center">
-        <div class="space-bottom">
-          <img src="/images/blue-collar-dev.png" class="logo" alt="" />
-        </div>
-      </div>-->
-
-      <div class="sm-flex flex-justify-between">
-        <div class="copyright g-footer">
-          <p class="g-pstyle15">&copy; Blue Collar Development Ltd. 2019<br /><small><small>Site design and development by Lucas Lopatka</small></small></p>
-        </div>
-        <div class="site-info g-footer">
-          <p class="g-pstyle14"><small>Terms & Conditions  |  Privacy Policy</small></p>
         </div>
       </div>
     </div>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="bg-black">
+  <main class="bg-evening-blue">
     <!--<intro></intro>-->
     <div :id="this.id" :class="`page-container container relative ${isSafari ? 'is-safari' : isChrome ? 'is-chrome' : ''}`">
       <div class="notifications" v-if="displayNotifications">
